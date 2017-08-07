89f6cc22760ea4509c086e1ca741e3626d2de88a
alansif <alansif@sina.com>
2017-08-08 00:49:00 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,96 @@
+<template>
+    <div id="app" style="width:100%;padding:10px 0;position:relative;">
+        <el-button type="primary" size="small" @click="refreshTable" style="margin-left:30px">刷新</el-button>
+        <el-pagination style="display:inline-block;vertical-align:middle;"
+                       @size-change="handleSizeChange"
+                       @current-change="handleCurrentChange"
+                       :current-page.sync="currentPage"
+                       :page-sizes="[10, 25, 50]"
+                       :page-size="pagesize"
+                       layout="total, sizes, prev, pager, next"
+                       :total="total">
+        </el-pagination>
+        <div style="padding:10px 30px">
+            <el-table :data="tableData" stripe>
+                <el-table-column prop="time" label="时间" :width="170" sortable>
+                    <template scope="scope">
+                        {{getTime(scope.row.time)}}
+                    </template>
+                </el-table-column>
+                <el-table-column prop="remoteip" label="IP" :width="130" sortable/>
+                <el-table-column prop="username" label="姓名" :width="100" sortable/>
+                <el-table-column prop="phone" label="电话" :width="130" sortable/>
+                <el-table-column prop="category" label="类别" :width="100" sortable/>
+                <el-table-column prop="message" label="内容"/>
+                <el-table-column prop="state" label="状态" :width="140" sortable>
+                    <template scope="scope">
+                        {{scope.row.state}}
+                        <el-switch v-model="scope.row.state" on-text="" off-text="" on-value="已处理" off-value="未处理" @change="handleStateChange(scope.$index, scope.row, $event)"/>
+                    </template>
+                </el-table-column>
+            </el-table>
+        </div>
+    </div>
+</template>
+
+<script>
+    import axios from 'axios'
+    import moment from 'moment'
+
+    export default {
+        data() {
+            return {
+                tableData: [],
+                currentPage: 1,
+                pagesize: 10,
+                total: 0
+            }
+        },
+        methods: {
+            refreshTable() {
+                axios({
+                    method: 'get',
+                    url: 'http://www.advahealth.com.cn:3939/api/v1/guestbook/be',
+                    params:{
+                        pagesize:this.pagesize,
+                        pageindex:this.currentPage
+                    }
+                }).then(response => {
+                    this.tableData = response.data.data;
+                    this.total = response.data.summary.total;
+                }).catch(error => {
+                    console.log(error);
+                });
+            },
+            getTime(v) {
+                return moment(v).format('YYYY-MM-DD HH:mm:ss');
+            },
+            handleSizeChange(val) {
+                this.pagesize = val;
+                this.refreshTable();
+            },
+            handleCurrentChange(val) {
+                this.refreshTable();
+            },
+            handleStateChange(index, row, evt) {
+                axios({
+                    method: 'post',
+                    url: `http://www.advahealth.com.cn:3939/api/v1/guestbook/be/${row.uid}/state`,
+                    data: { value: evt }
+                }).then(response => {
+                }).catch(error => {
+                    console.log(error);
+                });
+            }
+        },
+        mounted() {
+            this.refreshTable();
+        }
+    }
+</script>
+
+<style>
+    body {
+        font-family: Helvetica, sans-serif;
+    }
+</style>
